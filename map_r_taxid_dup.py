3d50e787fc744e975a234f1f3e2cb3d6afc6224c
Prasenjeet13 <34676964+Prasenjeet13@users.noreply.github.com>
2018-01-01 22:35:53 +0530

    Add files via upload

---
+++ map_r_taxid_dup.py
@@ -0,0 +1,28 @@
+from mrjob.job import MRJob
+from mrjob.job import MRStep
+
+class xyz(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer),
+            MRStep(mapper=self.r_output)
+            ]
+
+    def mapper(self,_,line):
+        (uid,tid,lid,x,y,z)=line.split(',')
+        yield uid,1
+
+    def reducer(self,uid,count):
+        #yield None,(sum(count),uid)->1
+        yield uid,sum(count)
+
+    def r_output(self,uid,s_count):
+        if(s_count>1):
+            yield uid,s_count
+        #yield max(s_count)->2 # This may give a result like 3    "ITE0002331" when used with 1
+
+
+if __name__ == '__main__':
+    xyz.run()
